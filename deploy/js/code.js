a8f97f4d779b177f6366d49945b3e66b736a4bec
John Doe <cjc10@192.168.1.115>
2016-06-04 15:02:38 -0500

    commit luego de la presentacion

--- deploy/js/code.js
+++ deploy/js/code.js
@@ -21,14 +21,13 @@ $(document).ready(function(){
     if(this.registros !== undefined){
       for (var i = this.registros.length - 1; i >= 0; i--) {
         if (this.registros[i].email === email && this.registros[i].password === password) {
-          console.log("bienvenido");
-          // alert("Bienvenido");
+          $("#bienvenida").show();
         }else {
-          alert("Por favor registrese");
+          $("#porfavorRegistrese").show();
         }   
       }
     }else{
-      alert("Registrate!!!!");
+      $("#porfavorRegistrese").show();
     }
   };
   
@@ -62,26 +61,28 @@ $(document).ready(function(){
       alert("No hay productos en tu lista de compras");
     };    
   };
- // ------------------------------Buscador
+ // --------------------------------------Buscador
   var Tienda = function(){
     this.inventario=[];
   };
   Tienda.prototype.agregarAMiBodega = function(producto){
     this.inventario.push(producto);
   }
-  // -------------Buscar
+  // ------------------------------------Funcion Buscar
   Tienda.prototype.buscar=function(entradaDeTexto){
       for (var i = this.inventario.length-1; i >= 0; i--){
         if(this.inventario[i].nombre === entradaDeTexto){
           alert(this.inventario[i].nombre+" "+"$ "+this.inventario[i].precio);
         }else{
-          alert("No se pudo encontrar este producto");
+          $("#noSeEncuentraProducto").show();
+          break;
         }
       };    
   };
-
+// -------------------------------------Definir Bodega de productos
   var miInventario= new Tienda;
   var miListaDeCompras=new CarritoDeCompras;
+
   var carroRC1= new Producto("Carro RC 1","buggie de carreras de alto cilindraje, de manejo complejo y alta aceleracion","$ " + 150000);
   $("#carroRC1-nombre").text(carroRC1.nombre);
   $("#carroRC1-descripcion").text(carroRC1.descripcion);
@@ -90,6 +91,7 @@ $(document).ready(function(){
     miListaDeCompras.agregarAlCarrito(carroRC1);
   });
   miInventario.agregarAMiBodega(carroRC1);
+
   var carroRC2= new Producto("Carro RC 2","Camioneta todo terreno con excelentes amortiguadores que garantizaran recorrer cualquier terreno","$ " + 180000);
   $("#carroRC2-nombre").text(carroRC2.nombre);
   $("#carroRC2-descripcion").text(carroRC2.descripcion);
@@ -98,6 +100,7 @@ $(document).ready(function(){
     miListaDeCompras.agregarAlCarrito(carroRC2);
   });
   miInventario.agregarAMiBodega(carroRC2);
+  
   var carroRC3= new Producto("Carro RC 3","buggie de alta estabilidad, con excelentes amortiguadores que permiten un menor manejo","$ " + 120000);
   $("#carroRC3-nombre").text(carroRC3.nombre);
   $("#carroRC3-descripcion").text(carroRC3.descripcion);
@@ -106,23 +109,32 @@ $(document).ready(function(){
     miListaDeCompras.agregarAlCarrito(carroRC3);
   });
   miInventario.agregarAMiBodega(carroRC3);
-  console.log(miInventario.inventario);
-  console.log(miListaDeCompras);
-
+// ----------------------------------Busqueda
+  $("#botonBusqueda").click(function(event){
+    event.preventDefault();
+    var miBusqueda = $("#busqueda").val();
+    miInventario.buscar(miBusqueda);
+  })
+  
   $("#logoCarrito").click(function(){
   alert(miListaDeCompras.listaDeCompras[0].nombre);
   });
-
+// -----------------------------------Comprobacion registro y login
+  var nuevoRegistro= new Inscripcion();
   $("#registrar").click(function(event){
     event.preventDefault();
+    
     var nuevoUsuario = new Persona ($("#inputNombre").val(),$("#inputApellido").val(),$("#inputEmail").val(),$("#inputPassword").val());
-       console.log(nuevoUsuario.firstName);
-       console.log(nuevoUsuario.lastName);
+      nuevoRegistro.registrar(nuevoUsuario);
+      console.log("hola");
+      console.log(nuevoRegistro.registros);
+      console.log(nuevoRegistro.registros[0].firstName);
+      console.log(nuevoRegistro.registros[0].lastName);
+
   });
   $("#login").click(function(event){
     event.preventDefault();
-    var nuevoLogin = new Inscripcion($("#loginEmail").val(),$("#loginPassword").val());
-    nuevoLogin.login();
+    nuevoRegistro.login($("#loginEmail").val(),$("#loginPassword").val());
   });
 
 });
